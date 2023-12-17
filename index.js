a06a184fc5bd4ddd37d0d127eaf1c53239eaf5d9
Abdirahman <abdirahman.abdi01@student.moringaschool.com>
2023-12-17 16:49:14 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(aim = "roller-skate") {
+    return `This Saturday, I want to ${aim}!`;
+  }
+  
+  function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  function wrapAdjective(flair = "*") {
+    return function (adjactive = "special") {
+      return `You are ${flair}${adjactive}${flair}!`;
+    };
+  }
